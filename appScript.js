2e20f0a109e79ed050703e40333f1a580c9c6ae7
Harold Diaz <harold.diazv1@gmail.com>
2022-09-16 10:56:03 -0500

    Adding slick slider for carousel images

--- appScript.js
+++ appScript.js
@@ -94,3 +94,27 @@ function showTime() {
   document.getElementById("minute").innerHTML = minutes;
   document.getElementById("sec").innerHTML = seconds;
 }
+
+// Slick slide
+
+$(".slider__top").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider__bottom",
+});
+$(".slider__bottom").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".slider__top",
+  dots: true,
+  // arrows: true,
+  focusOnSelect: true,
+});
+
+$(".slider__action button[data-slide]").click(function (e) {
+  e.preventDefault();
+  let slide = $(this).data("slide");
+  $(".slider__bottom").slick("slickGoTo", slide - 1);
+});
